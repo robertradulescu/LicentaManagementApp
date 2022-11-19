5acf250fe96cb27c9f91ff92e34facfe14eb9b1c
robertradulescu <robertvalentin11022000@gmail.com>
2022-11-19 20:19:59 +0200

    Installed Angular,BootStrap. We can data in browse

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+
+            services.AddCors(); //Cors suport pentru browser
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +46,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //for angular permision
 
             app.UseAuthorization();
 
